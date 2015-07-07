01bdd639ed61bb3c3b4eae1243243efb88ed616b
somu1795 <somu1795@gmail.com>
2015-07-08 03:08:25 +0530

    1st

---
+++ batterywin.py
@@ -0,0 +1,17 @@
+__author__ = 'Xplore'
+
+from ctypes import *
+
+class PowerClass(Structure):
+    _fields_ = [('ACLineStatus', c_byte),
+            ('BatteryFlag', c_byte),
+            ('BatteryLifePercent', c_byte),
+            ('Reserved1',c_byte),
+            ('BatteryLifeTime',c_ulong),
+            ('BatteryFullLifeTime',c_ulong)]    
+
+powerclass = PowerClass()
+result = windll.kernel32.GetSystemPowerStatus(byref(powerclass))
+
+#print(powerclass.BatteryLifePercent)
+
